0927e455fd4842b1f93d499aef36ccc5ca69e2f6
raddevus <1radeutsch1@gmail.com>
2020-06-17 19:21:24 -0400

    first commit of project

---
+++ webgetter.py
@@ -0,0 +1,21 @@
+import urllib.request
+import sys
+
+
+print("doing something")
+
+print(sys.argv)
+
+arguments = sys.argv
+argLength = len(sys.argv)
+
+print(argLength)
+
+if argLength < 2:
+    print("need more args")
+    exit()
+
+print ("you made it")
+
+webUrl = arguments[1]
+print(webUrl)
